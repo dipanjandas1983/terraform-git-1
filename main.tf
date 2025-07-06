4c0ec17d73619bd4bdfaa1b36fce983d8c894244
dipanjandas1983 <105868766+dipanjandas1983@users.noreply.github.com>
2025-07-06 17:18:08 +0530

    added 2 subnets in this file

--- main.tf
+++ main.tf
@@ -18,3 +18,20 @@ terraform {
     region = "us-east-1"
   }
 }
+
+# Public Subnet 1
+resource "aws_subnet" "public1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1d"
+  map_public_ip_on_launch = true
+}
+
+# Public Subnet 2
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1e"
+  map_public_ip_on_launch = true
+}
+
