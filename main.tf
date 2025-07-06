2c89f30c1051d446ca09b3928c4072a12fe19664
dipanjandas1983 <105868766+dipanjandas1983@users.noreply.github.com>
2025-07-07 01:49:18 +0530

    added 17 resources

--- main.tf
+++ main.tf
@@ -1,21 +1,35 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
-resource "aws_vpc" "main" {
-    cidr_block           = "10.0.0.0/16"
-    enable_dns_support   = true
-    enable_dns_hostnames = true
-
-    tags = {
-        Name = "main-vpc-1x"
-    }
-}
 terraform {
   backend "s3" {
     bucket = "tfstate-bucket-1x"
     key    = "infraupdate-1/terraform.tfstate"
     region = "us-east-1"
+    # Uncomment below line if you are using DynamoDB for state locking
+    # dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc-1"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
   }
 }
 
@@ -25,6 +39,10 @@ resource "aws_subnet" "public1" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1d"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
 }
 
 # Public Subnet 2
@@ -33,5 +51,192 @@ resource "aws_subnet" "public2" {
   cidr_block              = "10.0.4.0/24"
   availability_zone       = "us-east-1e"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
 }
 
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+# Route Table Associations
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# Target Group
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web-tg"
+  port     = 8080
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    port                = "8080"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+# Launch Template
+resource "aws_launch_template" "web" {
+  name_prefix   = "web-"
+  image_id      = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "web-server"
+    }
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              yum install -y httpd
+              sed -i 's/Listen 80/Listen 8080/' /etc/httpd/conf/httpd.conf
+              systemctl enable httpd
+              systemctl start httpd
+              echo "Hello from Terraform Web Server" > /var/www/html/index.html
+              EOF
+            )
+}
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "web_asg" {
+  desired_capacity    = 1
+  max_size            = 2
+  min_size            = 1
+  vpc_zone_identifier = [aws_subnet.public1.id, aws_subnet.public2.id]
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.web_tg.arn]
+  health_check_type = "EC2"
+
+  tag {
+    key                 = "Name"
+    value               = "web-server"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# Load Balancer
+resource "aws_lb" "web_lb" {
+  name               = "web-lb-1"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+  security_groups    = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "web-lb-1"
+  }
+}
+
+# Listener
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+
+# IAM User
+resource "aws_iam_user" "web_restart_user" {
+  name = "web-restart-user"
+}
+
+# IAM Policy
+resource "aws_iam_policy" "restart_policy" {
+  name        = "RestartWebServerPolicy"
+  description = "Allow restart of EC2 web server"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["ec2:RebootInstances"],
+        Resource = "*",
+        Condition = {
+          StringEquals = {
+            "ec2:ResourceTag/Name" = "web-server"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# IAM User Policy Attachment
+resource "aws_iam_user_policy_attachment" "attach_restart_policy" {
+  user       = aws_iam_user.web_restart_user.name
+  policy_arn = aws_iam_policy.restart_policy.arn
+}
